2597270e639a367fc387149f3b0372701f535df9
LudwigELATRE <73239553+LudwigELATRE@users.noreply.github.com>
2023-04-12 14:51:00 +0200

    suite preparation api

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -18,4 +18,16 @@ class BookController extends AbstractController
         $jsonBookList = $serializer->serialize($bookList, 'json');
         return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
     }
+
+
+    #[Route('/api/books/{id}', name: 'detailBook', methods: ['GET'])]
+    public function getBookById(int $id, BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $book = $bookRepository->findById($id);
+        if ($book) {
+            $jsonBook = $serializer->serialize($book, 'json');
+            return new JsonResponse($jsonBook, Response::HTTP_OK, [], true);
+        }
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+    }
 }
